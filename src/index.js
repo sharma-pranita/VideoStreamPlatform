12adf34a39dc873cea8417356feade90706d5527
sharma-pranita <sharmapranita707008@gmail.com>
2025-12-20 12:39:04 +0530

    Utility Folder

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ console.log(process.env.MONGODB_URL);
 import connectDB from "./db/index.js";
 
 connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("Error : ", err);
+        throw err; 
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);   
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed ", err);
+    
+})
 
 
 
